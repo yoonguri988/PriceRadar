bce4fd99447abc043e40b87792204810a8e452af
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-06-24 22:31:53 +0900

    Fixed: vercel Error2

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,22 +1,22 @@
-import { FilterOption, SortOption } from "@/features/product";
+// import { FilterOption, SortOption } from "@/features/product";
 
-export const SORT_OPTIONS: { label: string; value: SortOption }[] = [
-  { label: "전체 보기", value: "all" },
-  { label: "낮은 가격순", value: "priceAsc" },
-  { label: "높은 가격순", value: "priceDesc" },
-  // { label: "이름순", value: "name" },
-  // { label: "할인율순", value: "discount" },
-  { label: "신상품순", value: "latest" },
-];
+// export const SORT_OPTIONS: { label: string; value: SortOption }[] = [
+//   { label: "전체 보기", value: "all" },
+//   { label: "낮은 가격순", value: "priceAsc" },
+//   { label: "높은 가격순", value: "priceDesc" },
+//   // { label: "이름순", value: "name" },
+//   // { label: "할인율순", value: "discount" },
+//   { label: "신상품순", value: "latest" },
+// ];
 
-export const FILTER_OPTIONS: { label: string; value: FilterOption }[] = [
-  { label: "전체 카테고리", value: "all" },
-  { label: "전자제품", value: "electronics" },
-  { label: "패션", value: "fashion" },
-  { label: "뷰티", value: "beauty" },
-  { label: "스포츠", value: "sports" },
-  { label: "도서", value: "books" },
-];
+// export const FILTER_OPTIONS: { label: string; value: FilterOption }[] = [
+//   { label: "전체 카테고리", value: "all" },
+//   { label: "전자제품", value: "electronics" },
+//   { label: "패션", value: "fashion" },
+//   { label: "뷰티", value: "beauty" },
+//   { label: "스포츠", value: "sports" },
+//   { label: "도서", value: "books" },
+// ];
 
-// 한 페이지당 기준
-export const PAGE_STANDARD = 10;
+// // 한 페이지당 기준
+// export const PAGE_STANDARD = 10;
